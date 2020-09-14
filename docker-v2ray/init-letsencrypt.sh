07cfa32d5e98b3d9f2f93fac4fb95e64c87e9049
Tuwin <71218230+Tuwin@users.noreply.github.com>
2020-09-14 15:59:37 +0800

    Update init-letsencrypt.sh

--- docker-v2ray/init-letsencrypt.sh
+++ docker-v2ray/init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(relay.privacynom.cf)
+domains=(relay.pirvacynom.cf)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="vultruse@tuta.io" # Adding a valid address is strongly recommended
